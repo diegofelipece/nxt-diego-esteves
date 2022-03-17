35d3242d59797f9c99fab8ffcbefb707762e095c
Diego Esteves <diegofelipece@gmail.com>
2022-03-17 20:04:07 +0100

    chore: start using scss

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import styles from '../../styles/About.module.css'
+import styles from '../../styles/About.module.scss'
 
 const About: NextPage = () => {
   return (
