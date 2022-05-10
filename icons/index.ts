1b68a3ee855a6355b6fe1342b4837615d8225636
Diego Esteves <diegofelipece@gmail.com>
2022-05-10 23:27:00 +0200

    feat: start adding icons

---
+++ icons/index.ts
@@ -0,0 +1,19 @@
+import Android from './android.svg';
+import Apple from './apple.svg';
+import Behance from './behance.svg';
+import Facebook from './facebook.svg';
+import Github from './github.svg';
+import Link from './link.svg';
+import Linkedin from './linkedin.svg';
+import Tumblr from './tumblr.svg';
+
+export {
+  Android,
+  Apple,
+  Behance,
+  Facebook,
+  Github,
+  Link,
+  Linkedin,
+  Tumblr,
+}
